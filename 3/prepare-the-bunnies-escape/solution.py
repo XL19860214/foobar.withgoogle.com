e60d8a3241f1ad2cc7b88e0e5605dcead06654f7
Xuwei Li <dev@xuwei.li>
2021-10-26 00:45:10 +0000

    Solution for "Prepare the Bunnies' Escape" of level 3.

--- 3/prepare-the-bunnies-escape/solution.py
+++ 3/prepare-the-bunnies-escape/solution.py
@@ -2,92 +2,49 @@
 
 
 def solution(map):
-    path = findPath(map, True)
-    if path:
-        return len(path)
-    
-    paths = {}
-    for y, row in enumerate(map):
-        for x in row:
-            if map[y][x] == 1:
-                newMap = map.copy()
-                newMap[y][x] = 0
-                path = findPath(newMap)
-                if path:
-                    paths[len(path)] = path
-    print(paths) #DEBUG
-
-    return min(paths)
-
-
-def findPath(map, optimize = False):
-    history = []
-    blacklist = []
-    w = len(map[0])
     h = len(map)
-    print('w', w, 'h', h) #DEBUG
-    x = 0
-    y = 0
-    history.append((x, y))
-    while history[len(history) - 1][0] != w - 1 or history[len(history) - 1][1] != h - 1:
-        if x + 1 < w and map[y][x + 1] == 0 and (x + 1, y) not in blacklist and (x + 1, y) not in history:
-            x += 1
-        elif y + 1 < h and map[y + 1][x] == 0 and (x, y + 1) not in blacklist and (x, y + 1) not in history:
-            y += 1
-        elif x - 1 >= 0 and map[y][x - 1] == 0 and (x - 1, y) not in blacklist and (x - 1, y) not in history:
-            x -= 1
-        elif y - 1 >= 0 and map[y - 1][x] == 0 and (x, y - 1) not in blacklist and (x, y - 1) not in history:
-            y -= 1
-        else: # Fallback
-            last = history.pop()
-            blacklist.append(last)
-            print('Blacklist', last) #DEBUG
-            if last == (0, 0):
-                print('Must break wall.')
-                return False
-            fallback = history.pop()
-            x = fallback[0]
-            y = fallback[1]
+    w = len(map[0])
+    map1 = distantMap(map, (0, 0))
+    map2 = distantMap(map, (w - 1, h - 1))
+    # print(map)  #DEBUG
+    # print(map1) #DEBUG
+    # print(map2) #DEBUG
 
-        history.append((x, y))
-        print('Move to', (x, y)) #DEBUG
-    
-    print(history) #DEBUG
+    p = w * h
+    for x in range(w):
+        for y in range(h):
+            if map1[y][x] and map2[y][x]:
+                p = min(map1[y][x] + map2[y][x] - 1, p)
 
-    if optimize:
-        #
-        walls = {}
-        for i, pos in enumerate(history):
-            x_ = pos[0]
-            y_ = pos[1]
-            for j in range(0, i)[::-1]:
-                # print(pos, j, (history[j])) #DEBUG
-                if (history[j][0] + 2, history[j][1]) == (x_, y_):
-                    if map[y_][x_ - 1] == 1:
-                        wall = (x_ - 1, y_)
-                        walls[(j, i)] = wall
-                elif (history[j][0], history[j][1] + 2) == (x_, y_):
-                    if map[y_ - 1][x_] == 1:
-                        wall = (x_, y_ - 1)
-                        walls[(j, i)] = wall
-                elif (history[j][0] - 2, history[j][1]) == (x_, y_):
-                    if map[y_][x_ + 1] == 1:
-                        wall = (x_ + 1, y_)
-                        walls[(j, i)] = wall
-                elif (history[j][0], history[j][1] - 2) == (x_, y_):
-                    if map[y_ + 1][x_] == 1:
-                        wall = (x_, y_ + 1)
-                        walls[(j, i)] = wall
-        
-        longest = None
-        for range_ in walls.keys():
-            if longest == None or range_[1] - range_[0] > longest[1] - longest[0]:
-                longest = range_
-            
-        if walls:
-            print('walls', walls) #DEBUG
-            history[longest[0]:longest[1]] = walls[longest]
+    if p == w * h:
+        return False
+    return p
 
-        print(history) #DEBUG
 
-    return history
+def distantMap(map, coordinate):
+    h = len(map)
+    w = len(map[0])
+
+    # m = [[None] * w] * h
+    m = [[None for i in range(w)] for i in range(h)]
+    x, y = coordinate
+    m[y][x] = 1
+
+    moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+    queue = [coordinate]
+    while len(queue) > 0:
+        posX, posY = queue.pop(0)
+        for move in moves:
+            toX, toY = posX + move[0], posY + move[1]
+            # print('from', pos, 'to', (toX, toY)) #DEBUG
+            if 0 <= toX < w and 0 <= toY < h:
+                if m[toY][toX] is None:
+                    m[toY][toX] = m[posY][posX] + 1
+                    # Disrupt at a wall
+                    if map[toY][toX] == 1:
+                        continue
+                    queue.append((toX, toY))
+                    # print((toX, toY)) #DEBUG
+
+    return m
